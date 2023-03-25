b0e2f6119cb3e1334e8d2e7193d9e3ff91da2f05
ITpapers <vladislaw.boycko@gmail.com>
2023-03-25 15:19:59 -0700

    Five Commit: Multpl Inheritance, Requests

--- classinheritance.py
+++ classinheritance.py
@@ -1,19 +1,48 @@
 #class Parent:
 #    classmethods and attrs
 
-#class Child(Parent):
+#class Parent2:
 #    classmethods and attrs
 
-class Class1:
-    var = 20
-    def __init__(self):
-        self.var = 10
+#class Child(Parent1,Parent2):
+#    classmethods and attrs
 
-class Class2(Class1:
-    def __init__(self):
-        print(self.var)
-        super().__init__()
-        print(self.var)
-        print(super().var)
+#class Class1:
+ #   var = 20
+ #   def __init__(self):
+  #      self.var = 10
 
-ccc = Class2()
+#class Class2(Class1):
+ #   def __init__(self):
+  #      print(self.var)
+   #     super().__init__()
+    #    print(self.var)
+     #   print(super().var)
+
+#ccc = Class2()
+
+
+class Computer:
+    def __init__(self, model, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = model
+        self.memory = 128
+    def calculate(self):
+        print("Calculating...")
+
+class Display:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.resolution = "4k"
+    def display(self):
+        print("I display the image")
+
+class SmartPhone(Display,Computer):
+    def print_info(self):
+        print(self.memory)
+        print(self.resolution)
+        print(self.model)
+
+
+iphone = SmartPhone(model="iPhone 15")
+iphone.print_info()
---
+++ libtest.py
@@ -0,0 +1,17 @@
+import requests
+
+def first_function():
+    pass
+
+class Human:
+    pass
+
+rq = requests
+first_f = first_function
+nick = Human()
+
+intro_list = []
+for method in dir():
+    print(method)
+
+
